baa179dac8c08d48418f64714f13ce206d1b7270
hakan karaahmetoglu <ext1482@foss.dk>
2023-01-15 20:33:04 +0300

    router structure is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import "./App.scss";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+//components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+//pages
+import Home from "./components/Home";
+import MovieDetail from "./components/MovieDetail";
+import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/common/apis/MovieApiKey.ts
@@ -0,0 +1,1 @@
+export const APIKey = "46d26031-ba2f-4da0-8893-a77dca17f91a";
